f0023f4b387d5ac5246d6af38e1d54e6a1f738f9
kwatch <kwatch@gmail.com>
2024-10-08 10:33:35 +0900

    rewrite 'task/release_task.rb' entirely

--- rake/release_task.rb
+++ rake/release_task.rb
@@ -3,78 +3,101 @@
 
 
 ##
-## howto
+## release:howto
 ##
-
 desc "show how to release"
-task :howto do
-  puts <<'END'
-How to release:
+task "release:howto", [:version] do |t, args|
+  version = args[:version] || ENV['version']  or
+    fail "[ERROR] Version number required (such as 'version=1.0.0')."
+  puts howto_release(PROJECT, version)
+end
 
-  $ git diff                     # confirm that no diff
-  $ rake test
-  $ rake prepare release=0.0.0   # update release number
-  $ rake package                 # create gem file
-  $ rake release                 # upload to rubygems.org
-  $ git checkout .               # reset release number
-  $ git tag | grep 0.0.0         # confirm release tag
-  release-0.0.0
-  $ git push --tags
+def howto_release(project, version)
+  version =~ /\A(\d+\.\d+)/  or
+    abort "#{version}: Invalid version number."
+  ver = $1
+  zero_p = ver.end_with?('.0')
+  opt_b = zero_p ? " -b" : ""
+  comm  = zero_p ? "create a new" : "switch to existing"
+  return <<"END"
+## How to release #{version}
+
+git diff                	# confirm that there is no changes
+rake test
+rake test:all           	# test on Ruby 2.x ~ 3.x
+git checkout#{opt_b} rel-#{ver}    	# #{comm} release branch
+vi CHANGES.md
+git add CHANGES.md
+git commit -m "Update 'CHANGES.md'"
+git log -1              	# confirm the commit
+cid=$(git log -1 | awk 'NR==1{print $2}')  	# in order to cherry-pick later
+rake prepare[#{version}]        	# update release number in files
+git add -u .            	# add changes into staging area
+git status -sb .        	# list files in staging area
+git diff --cached       	# confirm changes in staging area
+git commit -m "Preparation for release #{version}"
+gem build #{project}.gemspec  	# build gem package
+gem unpack #{project}-#{version}.gem 	# extract gem package
+find #{project}-#{version}         	# confirm file list in gem package
+rm -rf #{project}-#{version}       	# delete extracted files
+gem push #{project}-#{version}.gem 	# release gem package
+git push -u origin rel-#{ver}
+git tag v#{version}             	# add version tag
+git push --tags
+git checkout -          	# back to main branch
+git log -1 $cid
+git cherry-pick $cid    	# apply the commit to update CHANGES.md
+git rm #{project}-#{version}.gem   	# if necessary
 
 END
 end
 
 
 ##
-## build
+## prepare
 ##
-
-desc "update release number"
-task :prepare do
-  version = version_number_required(:prepare)
-  edit(spec.files) {|s|
-    s.gsub(/\$Release\:.*?\$/,   "$Release\: #{release} $") \
-     .gsub(/\$Release\$/,        release)
+desc "update version number"
+task :prepare, [:version] do |t, args|
+  version = args[:version] || ENV['version']  or
+    fail "[ERROR] Version number required (such as 'version=1.0.0')."
+  copyright = COPYRIGHT
+  spec = load_gemspec_file("#{PROJECT}.gemspec")
+  edit_files(spec.files) {|s, fpath|
+    s = s.gsub(/\$Version\:.*?\$/,   "$Version\: #{version} $")
+    s = s.gsub(/\$Version\$/,        version)
+    s = s.gsub(/\$Copyright:.*?\$/,  "$Copyright\: #{copyright} $")
+    s = s.gsub(/\$Copyright\$/,      copyright)
+    if fpath == "MIT-LICENSE"
+      if copyright =~ /(\(c\).*)/
+        x = $1
+        s = s.sub(/^Copyright .*$/, "Copyright #{x}")
+      end
+    end
+    s
   }
 end
 
-namespace :gem do
-
-  desc "create gem package"
-  task :build do
-    sh "gem build #{PROJECT}.gemspec"
+def edit_files(*filepaths)
+  filepaths.flatten.each do |fpath|
+    next unless File.file?(fpath)
+    changed = edit_file(fpath)
+    puts "[Change] #{fpath}" if changed
   end
-
-  desc "upload gem to rubygems.org"
-  task :publish => :build do
-    spec = load_gemspec_file("#{PROJECT}.gemspec")
-    version = spec.version
-    gemfile = "#{PROJECT}-#{version}.gem"
-    print "*** Are you sure to upload #{gemfile}? [y/N]: "
-    answer = gets().strip()
-    if answer =~ /\A[yY]/
-      sh "gem push #{gemfile}"
-      #sh "git tag release-#{version}"
-      sh "git tag v#{version}"
-    end
-  end
-
 end
 
-def edit(*filepaths)
-  filepaths.flatten.each do |fpath|
-    next if ! File.file?(fpath)
-    File.open(fpath, 'r+b:utf-8') do |f|
-      s = f.read()
-      new_s = yield s
-      if new_s != s
-        f.rewind()
-        f.truncate(0)
-        f.write(new_s)
-        puts "[Change] #{fpath}"
-      end
+def edit_file(fpath)
+  changed = false
+  File.open(fpath, 'r+b:utf-8') do |f|
+    s = f.read()
+    new_s = yield s, fpath
+    if new_s != s
+      f.rewind()
+      f.truncate(0)
+      f.write(new_s)
+      changed = true
     end
   end
+  return changed
 end
 
 def load_gemspec_file(gemspec_file)
@@ -82,18 +105,30 @@ def load_gemspec_file(gemspec_file)
   return Gem::Specification::load(gemspec_file)
 end
 
-def version_number_required(task_name)
-  version = ENV['version']
-  unless version
-    $stderr.puts <<"END"
+
 ##
-## ERROR: rake #{task_name}: requires 'version=X.X.X' option.
-##        For example:
-##           $ rake #{task_name} version=1.0.0
+## gem:build
 ##
-END
-    errmsg = "rake #{task_name}: requires 'version=X.X.X' option."
-    raise ArgumentError.new(errmsg)
+desc "create gem package"
+task "gem:build" do
+  sh "gem build #{PROJECT}.gemspec"
+end
+
+
+##
+## gem:upload
+##
+desc "upload gem to rubygems.org"
+task "gem:publish" do
+  spec = load_gemspec_file("#{PROJECT}.gemspec")
+  version = spec.version.to_s
+  gemfile = "#{PROJECT}-#{version}.gem"
+  File.exist?(gemfile)  or
+    abort "[ERROR] Gem file (#{gemfile}) not found. Run 'rake gem:build' beforehand."
+  print "*** Are you sure to upload #{gemfile}? [y/N]: "
+  answer = $stdin.gets().strip()
+  if answer =~ /\A[yY]/
+    sh "gem push #{gemfile}"
+    sh "git tag v#{version}"
   end
-  return version
 end
