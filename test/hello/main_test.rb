139602171760e66e5be2e99c80e44ff93e2a858f
kwatch <kwatch@gmail.com>
2024-10-08 14:38:40 +0900

    update test scripts

--- test/hello/main_test.rb
+++ test/hello/main_test.rb
@@ -18,30 +18,22 @@ describe Hello::Main do
       assert_output(expected) { Hello::Main.new.run("Taki") }
     end
 
-
     context "when '-h' or '--help' specified" do
 
       it "prints help message to stdout." do
         expected = <<"END"
-Usage: #{File.basename($0)} [OPTIONS] [NAME]
-    -h, --help                       print help message
-    -v, --version                    print version
-    -n, --name=NAME                  user name
+Usage: #{File.basename($0)} [<options>] [<name>]
+
+Options:
+  -h, --help                print help message
+      --version             print version
+  -l, --lang=<lang>         language (en|fr|it)
 END
         assert_output(expected) { Hello::Main.new.run("-hv") }
         assert_output(expected) { Hello::Main.new.run("--help") }
       end
 
-      it "returns status code 0." do
-        status = nil
-        capture_io { status = Hello::Main.new.run("-hv") }
-        ok {status} == 0
-        capture_io { status = Hello::Main.new.run("--help") }
-        ok {status} == 0
-      end
-
     end
-
 
     context "when '-v' or '--version' specified" do
 
@@ -51,52 +43,40 @@ END
         assert_output(expected) { Hello::Main.new.run("--version") }
       end
 
-      it "returns status code 0." do
-        status = nil
-        capture_io { status = Hello::Main.new.run("-vv") }
-        ok {status} == 0
-        capture_io { status = Hello::Main.new.run("--version") }
-        ok {status} == 0
+    end
+
+    context "when '-l <lang>' or '--lang=<lang>' specified" do
+
+      it "prints a message according to language." do
+        assert_output("Bonjour, World!\n") { Hello::Main.new.run("-l", "fr") }
+        assert_output("Chao, World!\n")    { Hello::Main.new.run("--lang=it") }
       end
 
     end
-
-
-    context "when '-n NAME' or '--name=NAME' specified" do
-
-      it "prints NAME instead of default name." do
-        assert_output("Hello, Taki!\n")    { Hello::Main.new.run("-n", "Taki") }
-        assert_output("Hello, Mitsuba!\n") { Hello::Main.new.run("--name=Mitsuba") }
-      end
-
-    end
-
 
   end
 
 
   describe '.main()' do
 
-    it "exists 0 when finished successfully." do
-      pr = proc { Hello::Main.main([]) }
-      ex = nil
-      output, errmsg = capture_io do
-        ex = ok {pr}.raise?(SystemExit)
+    it "returns 0 when finished successfully." do
+      ret = nil
+      sout, serr = capture_io() do
+        ret = Hello::Main.main([])
       end
-      ok {ex.status} == 0
-      ok {output} == "Hello, World!\n"
-      ok {errmsg} == ""
+      ok {ret}  == 0
+      ok {sout} == "Hello, World!\n"
+      ok {serr} == ""
     end
 
     it "exists 1 when command-option is worng." do
-      pr = proc { Hello::Main.main(['-x']) }
-      ex = nil
-      output, errmsg = capture_io do
-        ex = ok {pr}.raise?(SystemExit)
+      ret = nil
+      sout, serr = capture_io() do
+        ret = Hello::Main.main(['-x'])
       end
-      ok {ex.status} == 1
-      ok {output} == ""
-      ok {errmsg} == "#{File.basename($0)}: invalid option: -x\n"
+      ok {ret}  == 1
+      ok {sout} == ""
+      ok {serr} == "[ERROR] invalid option: -x\n"
     end
 
   end
