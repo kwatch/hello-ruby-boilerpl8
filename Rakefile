0b4b2f2b17cd4e6bb593671ac1b32f135009bbed
kwatch <kwatch@gmail.com>
2024-10-08 09:41:30 +0900

    define 'test:all' task

--- Rakefile
+++ Rakefile
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
-PROJECT  = "hello"
+PROJECT       = "hello"
+
+RUBY_VERSIONS = ['3.3', '3.2', '3.1', '3.0', '2.7', '2.6']  # for 'test:all' task
 
 task :default => :help   # or :test if you prefer
 
--- rake/test_task.rb
+++ rake/test_task.rb
@@ -10,3 +10,47 @@ desc "run test scripts"
 task :test do
   ruby "test/all.rb"
 end
+
+desc "run test scripts on each Ruby version"
+task "test:all" do
+  pairs = ruby_path_list(ruby_root_dir(), ruby_versions())
+  pairs.each do |(version, path)|
+    puts "\e[33m========== Ruby #{version} ==========\e[0m"
+    sh "#{path} test/all.rb" do end   # run forcedly
+    puts ""
+    sleep 0.2
+  end
+end
+
+def ruby_versions()
+  if ENV['RUBY_VERSIONS']
+    return ENV['RUBY_VERSIONS'].split(",")
+  elsif defined?(RUBY_VERSIONS)
+    return RUBY_VERSIONS
+  else
+    fail "RUBY_VERSIONS should be set for 'test:all' task."
+  end
+end
+
+def ruby_root_dir()
+  vs_home = ENV['VS_HOME'] || (defined?(VS_HOME) && VS_HOME) || nil
+  vs_home  or
+    fail "$VS_HOME should be set for 'test:all' task."
+  return "#{vs_home}/ruby"
+end
+
+def ruby_path_list(ruby_root_dir, ruby_versions)
+  return ruby_versions.collect {|version|
+    path = "#{ruby_root_dir}/#{version}/bin/ruby"
+    if ! File.exist?(path)
+      paths = Dir.glob("#{ruby_root_dir}/#{version}.*/bin/ruby")
+      ! paths.empty?  or
+        fail "Ruby #{version} not found."
+      path = paths.sort_by {|x|
+        x =~ /\/([^\/]+)\/bin\/ruby\z/
+        $1.split('.').collect {|s| s.to_i }
+      }.last
+    end
+    [version, path]
+  }
+end
