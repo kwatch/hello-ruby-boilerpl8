15247d8b72aca740381100bd9c08c9fab98ca784
kwatch <kwatch@gmail.com>
2024-10-09 20:36:26 +0900

    redefine 'test:all' task

--- rake/test_task.rb
+++ rake/test_task.rb
@@ -13,9 +13,8 @@ end
 
 desc "run test scripts on each Ruby version"
 task "test:all" do
-  pairs = ruby_path_list(ruby_root_dir(), ruby_versions())
-  pairs.each do |(version, path)|
-    puts "\e[33m========== Ruby #{version} ==========\e[0m"
+  header = "\e[33m========== Ruby %s ==========\e[0m"
+  each_ruby_path(ruby_root_dir(), ruby_versions(), header) do |version, path|
     sh "#{path} test/all.rb" do end   # run forcedly
     puts ""
     sleep 0.2
@@ -33,24 +32,34 @@ def ruby_versions()
 end
 
 def ruby_root_dir()
-  vs_home = ENV['VS_HOME'] || (defined?(VS_HOME) && VS_HOME) || nil
-  vs_home  or
-    fail "$VS_HOME should be set for 'test:all' task."
-  return "#{vs_home}/ruby"
+  rbenv_root = ENV['RBENV_ROOT'] || (defined?(RBENV_ROOT) && RBENV_ROOT) || nil
+  vs_home    = ENV['VS_HOME'] || (defined?(VS_HOME) && VS_HOME) || nil
+  rbenv_dir  = File.expand_path("~/.rbenv")
+  if rbenv_root && ! rb_env_root.empty?
+    return "#{rbenv_root}/versions"
+  elsif vs_home && ! vs_home.empty?
+    return "#{vs_home}/ruby"
+  elsif File.directory?(rbenv_dir)
+    return "#{rbenv_dir}/versions"
+  else
+    fail "$RBENV_ROOT or $VS_HOME should be set for 'test:all' task."
+  end
 end
 
-def ruby_path_list(ruby_root_dir, ruby_versions)
-  return ruby_versions.collect {|version|
+def each_ruby_path(ruby_root_dir, ruby_versions, header, &block)
+  ruby_versions.each do |version|
+    puts header % version
     path = "#{ruby_root_dir}/#{version}/bin/ruby"
     if ! File.exist?(path)
-      paths = Dir.glob("#{ruby_root_dir}/#{version}.*/bin/ruby")
+      path_pat = "#{ruby_root_dir}/#{version}.*/bin/ruby"
+      paths = Dir.glob(path_pat)
       ! paths.empty?  or
-        fail "Ruby #{version} not found."
+        fail "Ruby #{version} not found. (#{path_pat})"
       path = paths.sort_by {|x|
         x =~ /\/([^\/]+)\/bin\/ruby\z/
         $1.split('.').collect {|s| s.to_i }
       }.last
     end
-    [version, path]
-  }
+    yield version, path
+  end
 end
